c0beda1dcb89f5775061f2024b222d70f4be0706
kjtkanaly <kjtkanaly@gmail.com>
2022-08-23 15:15:43 -0400

    working on CheckInclusion

---
+++ CheckInclusion.cs
@@ -0,0 +1,103 @@
+public class Solution {
+    public bool CheckInclusion(string s1, string s2) {
+    
+        List<char> c1Right = s1.ToArray().ToList();
+        List<char> c1Left = s1.ToArray().ToList();
+        
+        c1Right.RemoveAt(0);
+		c1Left.RemoveAt(0);
+        
+        char firstLetter = s1[0];
+        
+        int start = s2.IndexOf(firstLetter, 0);
+        
+        //Debug
+        Console.WriteLine(start);
+        
+        // Sliding to the right
+        while (start != -1)
+        {
+            c1Right = s1.ToArray().ToList();
+            c1Right.RemoveAt(0);
+            
+            // Check to the right of start
+            int right = start + 1;
+            while((c1Right.Count > 0) && (right < s2.Length))
+            {
+                if (c1Right.Contains(s2[right]))
+                {
+                    c1Right.Remove(s2[right]);
+                }
+                else
+                {
+                    break;
+                }
+                right++;
+            }
+            
+            if (c1Right.Count == 0)
+            {
+                return true;
+            }
+            
+			c1Left = s1.ToArray().ToList();
+			c1Left.RemoveAt(0);
+            
+            // Check to the left of start
+            int left = start - 1; 
+            while((c1Left.Count > 0) && (c1Right.Count > 0) && (left > -1))
+            {
+                Console.WriteLine("C1 Left Count: " + c1Left.Count);
+                
+                bool RightBreakFlag = false;
+                
+                if (c1Right.Contains(s2[left]))
+                {
+                    c1Right.Remove(s2[left]);
+                }
+                else
+                {
+                    RightBreakFlag = true;
+                }
+                
+				if (c1Left.Contains(s2[left]))
+				{
+					c1Left.Remove(s2[left]);
+				}
+                else
+                {
+                    if (RightBreakFlag == true)
+                    {
+                        break;
+                    }
+                }
+				
+                left--;
+            }
+            
+            
+            if ((c1Right.Count == 0) || (c1Left.Count == 0))
+            {
+                return true;
+            }
+            
+            start = s2.IndexOf(firstLetter, start + 1);
+            
+            //Debug
+            Console.WriteLine(start);
+        }
+        
+        return false;
+    }
+}
+
+/*
+"abc"
+"cccccbabbbaaaa"
+"abc"
+"bbbca"
+"ab"
+"eidbaooo"
+"ab"
+"eidboaoo"
+*/
