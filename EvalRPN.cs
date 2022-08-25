75b1ad321eb1850eb866ff3a4fb087d07f8c2e7e
kjtkanaly <kjtkanaly@gmail.com>
2022-08-25 08:14:01 -0400

    Given a string of tokens in reverse polish notation, return the proper int that would result from the expression

--- EvalRPN.cs
+++ EvalRPN.cs
@@ -14,35 +14,44 @@ public class Solution {
         while (index < t.Count)
         {
             if (validOp.Contains(t[index]))
-            {
+            {                
                 var numb1 = Convert.ToInt32(t[index - 2]);
                 var numb2 = Convert.ToInt32(t[index - 1]);
                 
-                t.RemoveRange(index - 2, 2);
-                
                 if (t[index] == validOp[0])
                 {
-                    t.Insert(index, (char)(numb1 + numb2) + "");
+                    t.Insert(index - 2, (numb1 + numb2) + "");
+                    t.RemoveRange(index - 1, 3);
                 }
                 
-                break;
+                else if (t[index] == validOp[1])
+                {
+                    t.Insert(index - 2, (numb1 - numb2) + "");
+                    t.RemoveRange(index - 1, 3);
+                }
                 
-                t.RemoveAt(index + 1);
+                else if (t[index] == validOp[2])
+                {
+                    t.Insert(index - 2, (numb1 * numb2) + "");
+                    t.RemoveRange(index - 1, 3);
+                }
                 
+                else if (t[index] == validOp[3])
+                {
+                    t.Insert(index - 2, (numb1 / numb2) + "");
+                    t.RemoveRange(index - 1, 3);
+                } 
                 
+                //break;
+                index = 0;
             }
+            
             else
             {
                 index++;   
-            }
+            }            
         }
         
-        // Debug
-        for (int i = 0; i < t.Count; i++)
-        {
-            Console.WriteLine(t[i]);
-        }
-        
-        return 0;
+        return Convert.ToInt32(t[0]);
     }
 }
