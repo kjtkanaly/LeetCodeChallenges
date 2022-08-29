52da6e449d7c2fee8cfed36540cf10c3850c8319
kjtkanaly <kjtkanaly@gmail.com>
2022-08-29 09:19:46 -0400

    Returns the reverse of a linked node list

---
+++ ReverseList.cs
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        
+        ListNode PreviousNode = null, CurrentNode = head, NextNode;
+        
+        while (CurrentNode != null)
+        {
+            NextNode = CurrentNode.next;
+            CurrentNode.next = PreviousNode;
+            PreviousNode = CurrentNode;
+            CurrentNode = NextNode;
+        }
+        
+            
+        return PreviousNode;
+    }
+    
+
+}
