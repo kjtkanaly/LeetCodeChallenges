043672dc9af43dfbfb7044a9cb25a69991d28f5b
kjtkanaly <kjtkanaly@gmail.com>
2022-09-02 14:11:54 -0400

    Work in Progress

--- FindWords.cs
+++ FindWords.cs
@@ -9,66 +9,72 @@ public class Solution {
         // Iterate through the words
         for (int i = 0; i < words.Length; i++)
         {
-            int[] Cord = searchTwoDimArray(board, words[i][0]);
+            List<int[]> Cords = searchTwoDimArray(board, words[i][0]);
+			Console.WriteLine("Cord Length: " + Cords.Count);
             
-            if (Cord[0] != -1 && Cord[1] != -1)
+            int j = 0;
+            if (Cords.Count > 0)
             {
-                Console.WriteLine("Target Found: " + Cord[0] + "," + Cord[1]);
-                
-                int CharCount = 1;
-                while (CharCount < words[i].Length)
+                for (int CordCount = 0; CordCount < Cords.Count; CordCount++)
                 {
-                    char target = words[i][CharCount];
-                    
-                    // Check to the left
-                    if (Cord[1] > 0)
-                    {
-                        if (board[Cord[0]][Cord[1] - 1] == target)
-                        {
-                            Cord[1] -= 1;
-                            CharCount++;
-                        }
-                    }
-                    
-                    // Check to the right
-                    if (Cord[1] < board[0].GetLength(0) - 1)
-                    {
-                        if (board[Cord[0]][Cord[1] + 1] == target)
-                        {
-                            Cord[1] += 1;
-                            CharCount++;
-                        }
-                    }
-                    
-                    // Check Up
-                    if (Cord[0] > 0)
-                    {
-                        if (board[Cord[0] - 1][Cord[1]] == target)
-                        {
-                            Cord[0] -= 1;
-                            CharCount++;
-                        }
-                    }
-                    
-                    // Check Down
-                    if (Cord[1] < board.GetLength(0) - 1)
-                    {
-                        if (board[Cord[0] + 1][Cord[1]] == target)
-                        {
-                            Cord[0] += 1;
-                            CharCount++;
-                        }
-                    }
-                    
-                    if (board[Cord[0]][Cord[1]] != target)
-                    {
-                        break;
-                    }
-                }
+                    Console.WriteLine("Target Found: " + Cords[j][CordCount] + "," + Cords[j][1]);
                 
-                if (CharCount == words[i].Length)
-                {
-                    Output.Add(words[i]);
+                    int CharCount = 1;
+                    while (CharCount < words[i].Length)
+                    {
+                        char target = words[i][CharCount];
+
+                        // Check to the left
+                        if (Cords[j][1] > 0)
+                        {
+                            if (board[Cords[j][CordCount]][Cords[j][1] - 1] == target)
+                            {
+                                Cords[j][1] -= 1;
+                                CharCount++;
+                            }
+                        }
+
+                        // Check to the right
+                        if (Cords[j][1] < board[CordCount].GetLength(0) - 1)
+                        {
+                            if (board[Cords[j][CordCount]][Cords[j][1] + 1] == target)
+                            {
+                                Cords[j][1] += 1;
+                                CharCount++;
+                            }
+                        }
+
+                        // Check Up
+                        if (Cords[j][CordCount] > 0)
+                        {
+                            if (board[Cords[j][CordCount] - 1][Cords[j][1]] == target)
+                            {
+                                Cords[j][CordCount] -= 1;
+                                CharCount++;
+                            }
+                        }
+
+                        // Check Down
+                        if (Cords[j][1] < board.GetLength(0) - 1)
+                        {
+                            if (board[Cords[j][CordCount] + 1][Cords[j][1]] == target)
+                            {
+                                Cords[j][CordCount] += 1;
+                                CharCount++;
+                            }
+                        }
+
+                        if (board[Cords[j][CordCount]][Cords[j][1]] != target)
+                        {
+                            break;
+                        }
+                    }
+
+                    if (CharCount == words[i].Length)
+                    {
+                        Output.Add(words[i]);
+                    }
+                    
                 }
             }
         }
@@ -76,22 +82,22 @@ public class Solution {
         return Output;
     }
     
-    public int[] searchTwoDimArray(char[][] board, char target)
+    public List<int[]> searchTwoDimArray(char[][] board, char target)
     {
-        int row = -1, col = -1;
+        List<int[]> Output = new List<int[]>();
         
-        for (int i = 0; i < board.GetLength(0); i++)
+        for (int row = 0; row < board.GetLength(0); row++)
         {
-            int index = Array.IndexOf(board[i], target);
+            int col = Array.IndexOf(board[row], target);
             
-            if (index != -1)
+            while (col != -1)
             {
-                row = i;
-                col = index;
-                break;
+                Output.Add(new int[]{row,col});
+                
+                col = Array.IndexOf(board[row], target, col + 1);
             }
         }
         
-        return new int[]{row,col};
+        return Output;
     }
 }
