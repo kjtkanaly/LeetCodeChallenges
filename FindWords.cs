2287df9ae7cf25b1a79bcfbb67535a29ee256d5a
kjtkanaly <kjtkanaly@gmail.com>
2022-09-02 15:04:05 -0400

    Work in Progress

--- FindWords.cs
+++ FindWords.cs
@@ -3,76 +3,79 @@ public class Solution {
         
         IList<string> Output = new List<string>();
         
-        Console.WriteLine("# of Rows: " + board.GetLength(0));
-        Console.WriteLine("# of Cols: " + board[0].GetLength(0));
+        //Console.WriteLine("# of Rows: " + board.GetLength(0));
+        //Console.WriteLine("# of Cols: " + board[0].GetLength(0));
         
         // Iterate through the words
-        for (int i = 0; i < words.Length; i++)
+        for (int WordCount = 0; WordCount < words.Length; WordCount++)
         {
-            List<int[]> Cords = searchTwoDimArray(board, words[i][0]);
+            List<int[]> Cords = searchTwoDimArray(board, words[WordCount][0]);
+            Console.Write("\n");
+            Console.WriteLine("Word: " + words[WordCount]);
 			Console.WriteLine("Cord Length: " + Cords.Count);
             
-            int j = 0;
             if (Cords.Count > 0)
             {
                 for (int CordCount = 0; CordCount < Cords.Count; CordCount++)
                 {
-                    Console.WriteLine("Target Found: " + Cords[j][CordCount] + "," + Cords[j][1]);
+                    Console.WriteLine("Target Found: " + Cords[CordCount][0] + "," + Cords[CordCount][1]);
                 
                     int CharCount = 1;
-                    while (CharCount < words[i].Length)
+                    while (CharCount < words[WordCount].Length)
                     {
-                        char target = words[i][CharCount];
+						Console.WriteLine("CharCount: " + CharCount);
+                        char target = words[WordCount][CharCount];
 
                         // Check to the left
-                        if (Cords[j][1] > 0)
+                        if (Cords[CordCount][1] > 0)
                         {
-                            if (board[Cords[j][CordCount]][Cords[j][1] - 1] == target)
+                            if (board[Cords[CordCount][0]][Cords[CordCount][1] - 1] == target)
                             {
-                                Cords[j][1] -= 1;
+                                Cords[CordCount][1] -= 1;
                                 CharCount++;
                             }
                         }
 
                         // Check to the right
-                        if (Cords[j][1] < board[CordCount].GetLength(0) - 1)
+                        if (Cords[CordCount][1] < board[CordCount].GetLength(0) - 1)
                         {
-                            if (board[Cords[j][CordCount]][Cords[j][1] + 1] == target)
+                            if (board[Cords[CordCount][0]][Cords[CordCount][1] + 1] == target)
                             {
-                                Cords[j][1] += 1;
+                                Cords[CordCount][1] += 1;
                                 CharCount++;
                             }
                         }
 
                         // Check Up
-                        if (Cords[j][CordCount] > 0)
+                        if (Cords[CordCount][0] > 0)
                         {
-                            if (board[Cords[j][CordCount] - 1][Cords[j][1]] == target)
+                            if (board[Cords[CordCount][0] - 1][Cords[CordCount][1]] == target)
                             {
-                                Cords[j][CordCount] -= 1;
+                                Cords[CordCount][0] -= 1;
                                 CharCount++;
                             }
                         }
 
                         // Check Down
-                        if (Cords[j][1] < board.GetLength(0) - 1)
+                        if (Cords[CordCount][1] < board.GetLength(0) - 1)
                         {
-                            if (board[Cords[j][CordCount] + 1][Cords[j][1]] == target)
+                            if (board[Cords[CordCount][0] + 1][Cords[CordCount][1]] == target)
                             {
-                                Cords[j][CordCount] += 1;
+                                Cords[CordCount][0] += 1;
                                 CharCount++;
                             }
                         }
-
-                        if (board[Cords[j][CordCount]][Cords[j][1]] != target)
+						Console.WriteLine(board[Cords[CordCount][0]][Cords[CordCount][1]]);
+                        if (board[Cords[CordCount][0]][Cords[CordCount][1]] != target)
                         {
                             break;
                         }
                     }
 
-                    if (CharCount == words[i].Length)
+                    if (CharCount == words[WordCount].Length)
                     {
-                        Output.Add(words[i]);
+                        Output.Add(words[WordCount]);
+                        break;
                     }
                     
                 }
