0ae1dc4512fda30499246febb80b49b8573008a0
kjtkanaly <kjtkanaly@gmail.com>
2022-09-15 15:13:58 -0400

    Work in progress

--- EraseOverlapIntervals.cs
+++ EraseOverlapIntervals.cs
@@ -1,22 +1,67 @@
 public class Solution {
     public int EraseOverlapIntervals(int[][] intervals) {
-     
+        
         List<int> Left  = new List<int>(intervals.Length);
         List<int> Right = new List<int>(intervals.Length);
+        int DeleteCount = 0;
         
         for (int i = 0; i < intervals.Length; i++)
         {
-            Left.Add(intervals[i][0]);
-            Right.Add(intervals[i][1]);
+            if (Left.Contains(intervals[i][0]) && Right.Contains(intervals[i][1]))
+            {
+                DeleteCount++;
+            }
+            else
+            {
+                Left.Add(intervals[i][0]);
+                Right.Add(intervals[i][1]);
+            }
         }
         
-        List<int> LeftDist  = Left.Distinct().ToList();
-        List<int> RightDist = Right.Distinct().ToList();
+        while (true)
+        {
+            int Index = 0;
+            List<int> OverlapCount = new List<int>();
+            for (int i = 0; i < Left.Count; i++)
+            {
+                int Count = 0;
+                for (int j = 0; j < Left.Count; j++)
+                {
+                    if (i != j)
+                    {
+                        if (Left[i] < Right[j] && Right[i] > Left[j])
+                        {
+                            Count++;
+                        }
+                    }
+                }
+
+                Console.WriteLine("Overlap Count: " + Count);
+                OverlapCount.Add(Count);
+
+            }
+            
+            Console.Write("\n");
+            
+            if (OverlapCount.ToArray().Sum() == 0)
+            {
+                return DeleteCount;
+            }
+
+            else
+            {
+                //Console.Write("Beep: " + OverlapCount.IndexOf(OverlapCount.Max()) + "\n\n");
+                int index = OverlapCount.IndexOf(OverlapCount.Max());    
+                Right.RemoveAt(index);
+                Left.RemoveAt(index);
+                DeleteCount++;
+            }
+        }
         
-        int LeftDiff = Left.Count - LeftDist.Count;
-        int RightDiff = Right.Count - RightDist.Count;
         
-        return Math.Max(LeftDiff, RightDiff);
+        
+        
+        return 0;
         
         // Shuld Be doing.
         // Run Through each interval
