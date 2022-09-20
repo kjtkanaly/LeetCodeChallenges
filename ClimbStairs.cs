89ca63f0b639794188a6e8448b8ad434ff153b22
kjtkanaly <kjtkanaly@gmail.com>
2022-09-20 08:27:47 -0400

    Climbing a staircase takes 'n' steps to reach the top. Each step you can take either 1 or 2 stpes. How many ways can you climb the steps

--- ClimbStairs.cs
+++ ClimbStairs.cs
@@ -1,30 +1,30 @@
 public class Solution {
     public int ClimbStairs(int n) {
         
-        // Defaults
+        // Default
         if (n == 1)
         {
             return 1;
         }
-        
-        if (n == 2)
+        else if (n == 2)
         {
             return 2;
         }
         
-        int first  = 1;
-        int second = 2;
-        int result = 0;
+        int DPTwo = 1;
+        int DPOne = 1;
         
-        for (int i = 2; i < n; i++)
-        {
-            result = first + second;
-            
-            first  = second;
-            second = result;
+        int Current = 0;
+        
+        for (int i = 2; i <= n; i++)
+        {            
+            Current = DPOne + DPTwo;
+
+            DPTwo = DPOne;
+            DPOne = Current;
         }
         
-        return result;
-        
+        return Current;
     }
+
 }
