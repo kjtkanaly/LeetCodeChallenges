cf014e4c932af976566331b9f270b4394cf7d52e
kjtkanaly <kjtkanaly@gmail.com>
2022-09-20 10:48:05 -0400

    Given an integer array 'cost'. Return the cheapest path to the end of the array if you are allowed to take one or two steps at a time

--- MinCostClimbingStairs.cs
+++ MinCostClimbingStairs.cs
@@ -1,66 +1,28 @@
 public class Solution {
+    
     public int MinCostClimbingStairs(int[] cost) {
         
-        if (cost[0] == 0)
+        // Default
+        if (cost.Length == 2)
         {
-            List<int> Cost = cost.ToList();
-            Cost.RemoveAt(0);
-            cost = Cost.ToArray();
+            return Math.Min(cost[0], cost[1]);
         }
-        
-        if (cost[cost.Length - 1] == 0)
-        {
-            List<int> Cost = cost.ToList();
-            Cost.RemoveAt(cost.Length - 1);
-            cost = Cost.ToArray();
-        }
-        
-        if (cost.Length == 3)
-        {
-            return Math.Min(cost[2]+cost[0], cost[1]);
-        }
-        
-        // Check for cheapest path to the left
-        int index = cost.Length;
-        int LeftTotal = 0;
-        
-        while (index >= 2)
-        {
-            if (cost[index - 1] < cost[index - 2])
-            {
-                index -= 1;
-                LeftTotal += cost[index];
-            }
-            else
-            {
-                index -= 2;
-                LeftTotal += cost[index];
-            }
             
-            Console.WriteLine("Index: " + index);
-        }
         
-        // Check for cheapest path to the right
-        index = -1;
-        int RightTotal = 0;
+        int DP_One = cost[1];
+        int DP_Two = cost[0];
         
-        while (index < cost.Length - 2)
+        int current = 0;
+        
+        for(int i = 2; i < cost.Length; i++)
         {
-            if (cost[index + 1] < cost[index + 2])
-            {
-                index += 1;
-                RightTotal += cost[index];
-            }
-            else
-            {
-                index += 2;
-                RightTotal += cost[index];
-            }
+            current = cost[i] + Math.Min(DP_One, DP_Two);
             
-            Console.WriteLine("Index: " + index);
+            DP_Two = DP_One;
+            DP_One = current;
         }
         
-        
-        return Math.Min(LeftTotal, RightTotal);
+        return Math.Min(DP_Two, current);
     }
+    
 }
