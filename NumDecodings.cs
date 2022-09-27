ff3c83d4d5c64bb9e28f6b935fc66ac0aecabd73
kjtkanaly <kjtkanaly@gmail.com>
2022-09-27 08:48:46 -0400

    Given a string of ints. Return the number of possible ways the string can be decoded

--- NumDecodings.cs
+++ NumDecodings.cs
@@ -1,89 +1,32 @@
 public class Solution {
-
-    public Dictionary<int, char> Match = new Dictionary<int, char>();
-    public List<string> Codes = new List<string>();
     
     public int NumDecodings(string s) {
         
-        // Default
-        if (s[0] == '0')
-        {
+        if (s[0] == '0') {
             return 0;
         }
         
-        // Convert s to int array
-        int[] SN = new int[s.Length];
-        for (int i = 0; i < s.Length; i++)
-        {
-            SN[i] = Int32.Parse(s[i] + "");
+        int n = s.Length;
+        
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            
+            int ones = Int32.Parse(s.Substring(i - 1, 1));
+            if (ones >= 1 && ones <= 9) {
+                dp[i] += dp[i - 1];
+            }
+            
+            int tens = Int32.Parse(s.Substring(i - 2, 2));
+            if (tens >= 10 && tens <= 26) {
+                dp[i] += dp[i - 2];
+            }
         }
         
-        // Letters Array
-        char[] Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
-        
-        // Populate the dictionary
-        for (int i = 1; i <= 26; i++)
-        {
-            Match.Add(i, Chars[i - 1]); 
-        }
-        
-        Split(SN, "");
-        //Console.Write("\n");
-        
-        Codes = Codes.Distinct().ToList();
-        
-        return Codes.Count;
+        return dp[n];
     }
     
-    public void Split(int[] SN, string Code)
-    {
-        if (SN.Length == 0)
-        {
-            Console.WriteLine(Code);
-            Codes.Add(Code);
-        }
-
-        // Single Branch
-        if (SN.Length > 0)
-        { 
-            // Single
-            //Console.WriteLine("Single: " + Match[SN[SN.Length - 1]] + Code);
-            try
-            {
-                string single = Match[SN[SN.Length - 1]] + Code;
-
-                if (SN.Length != 0)
-                {
-                    Split(SN[(0)..(SN.Length - 1)], single);   
-                }
-                
-
-            }
-            catch
-            {
-
-            }
-        }
-        
-        // Double Branch
-        if (SN.Length > 1)
-        {     
-            // Double
-            //Console.WriteLine("Double: " + Match[SN[SN.Length - 2] + SN[SN.Length - 1]] + Code);
-            try
-            {
-                string twice = Match[10 * SN[SN.Length - 2] + SN[SN.Length - 1]] + Code;
-
-                if (SN.Length > 1)
-                {
-                    Split(SN[(0)..(SN.Length - 2)], twice);   
-                }
-                
-            }
-            catch
-            {
-
-            }
-        }
-    }
+    
 }
