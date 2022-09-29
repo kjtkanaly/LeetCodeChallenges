93b195c539b0558713d378501765aa836219c6a5
kjtkanaly <kjtkanaly@gmail.com>
2022-09-29 13:19:18 -0400

    Given a non-empty array 'nums' containing only positive integers, find if the array can be partioned into two subsets with equal sums

---
+++ CanPartition.cs
@@ -0,0 +1,64 @@
+public class Solution {
+    public bool CanPartition(int[] nums) {
+        
+        // Default
+        if (nums.Length == 2)
+        {
+            if (nums[0] == nums[1])
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+        
+        int Sum = nums.Sum();
+        Console.WriteLine("Sum: " + Sum + "\n");
+        
+        List<int> SubSums = new List<int>();
+            
+        for (int Index = nums.Length - 1; Index > -1; Index--)
+        {            
+            int[] Previous = new int[SubSums.Count];
+            SubSums.CopyTo(Previous);
+            
+            for (int i = 0; i < Previous.Length; i++)
+            {
+                if (SubSums.Contains(Previous[i] + nums[Index]) == false)
+                {
+                    SubSums.Add(Previous[i] + nums[Index]);    
+                }
+            }
+            
+            SubSums.Add(nums[Index]);
+            
+            // Debug 
+            /*
+            Console.WriteLine("Start: " + nums[Index]);
+            Console.Write("SubSums: ");
+            for (int i = 0; i < SubSums.Count; i++)
+            {
+                Console.Write(SubSums[i] + ", ");
+            }
+            Console.Write("\n\n");
+            /**/
+            
+            
+            for (int i = 0; i < SubSums.Count; i++)
+            {
+                if (Sum - SubSums[i] == SubSums[i])
+                {
+                    return true;
+                }
+            }
+            
+            
+            
+        }
+            
+        return false;
+    }
+}
