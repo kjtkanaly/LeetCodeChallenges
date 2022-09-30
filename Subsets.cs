02b5669ac7a0657744e4ca90fb605379786c0706
kjtkanaly <kjtkanaly@gmail.com>
2022-09-30 10:24:48 -0400

    Given an int array nums of unique elements, return all possible subsets

--- Subsets.cs
+++ Subsets.cs
@@ -9,51 +9,22 @@ public class Solution {
         
         Tree(nums.ToList(), new List<int>());
         
-        IList<IList<int>> Output = new List<IList<int>>();
-        
-        for (int i = 0; i < Result.Count; i++)
-        {
-            Output.Add(Result[i]);
-            
-            for (int j = i + 1; j < Result.Count; j++)
-            {
-                if (i != j)
-                {
-                    var firstNotSecond = Result[i].Except(Result[j]).ToList();
-                    var secondNotFirst = Result[j].Except(Result[i]).ToList();
-
-                    if (firstNotSecond.Count == 0 && secondNotFirst.Count == 0)
-                    {
-                        Output.RemoveAt(Output.Count - 1);
-                        break;
-                    }
-                }
-            }
-            
-        }
-        
-        return Output;
+        return Result;
     }
     
-    public void Tree(List<int> Nums, List<int> Input, int OmitIndex = -1)
-    {
-        // Debug
+    public void Tree(List<int> Nums, List<int> Input, int StartIndex = 0)
+    {        
+        /* // Debug
         Console.WriteLine("Index: " + OmitIndex);
         Console.WriteLine("Input Len: " + Input.Count);        
         Console.WriteLine("Nums Len: " + Nums.Count + "\n");
         /**/
         
-        if (OmitIndex != -1)
-        {
-            //Input.Add(Nums[OmitIndex]);
-            //Nums.RemoveAt(OmitIndex);
-        }
-        
         Result.Add(Input);
           
         if (Input.Count < MaxLength)
         {
-            for (int i = 0; i < Nums.Count; i++)
+            for (int i = StartIndex; i < Nums.Count; i++)
             {   
                 List<int> NewInput = new List<int>();
                 NewInput.Add(Nums[i]);
@@ -61,12 +32,12 @@ public class Solution {
                 NewInput.AddRange(Input);
                 NewInput.Sort();
 
-                Tree(Nums.Where(x => x != Nums[i]).ToList(), NewInput);
+                Tree(Nums.Where(x => x != Nums[i]).ToList(), NewInput, i);
+                
+                //Nums = Nums.Where(x => x != Nums[i]).ToList();
             }
         }
-        
-        
-        
+
     }   
 
 }
