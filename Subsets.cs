9ee55380ebea023d13e9b5e975b3ea9502797c1f
kjtkanaly <kjtkanaly@gmail.com>
2022-09-30 09:50:15 -0400

    Given an int array nums of unique elements, return all possible subsets

--- Subsets.cs
+++ Subsets.cs
@@ -9,14 +9,39 @@ public class Solution {
         
         Tree(nums.ToList(), new List<int>());
         
-        return Result.Distinct().ToList();
+        IList<IList<int>> Output = new List<IList<int>>();
+        
+        for (int i = 0; i < Result.Count; i++)
+        {
+            Output.Add(Result[i]);
+            
+            for (int j = i + 1; j < Result.Count; j++)
+            {
+                if (i != j)
+                {
+                    var firstNotSecond = Result[i].Except(Result[j]).ToList();
+                    var secondNotFirst = Result[j].Except(Result[i]).ToList();
+
+                    if (firstNotSecond.Count == 0 && secondNotFirst.Count == 0)
+                    {
+                        Output.RemoveAt(Output.Count - 1);
+                        break;
+                    }
+                }
+            }
+            
+        }
+        
+        return Output;
     }
     
     public void Tree(List<int> Nums, List<int> Input, int OmitIndex = -1)
     {
+        // Debug
         Console.WriteLine("Index: " + OmitIndex);
         Console.WriteLine("Input Len: " + Input.Count);        
         Console.WriteLine("Nums Len: " + Nums.Count + "\n");
+        /**/
         
         if (OmitIndex != -1)
         {
@@ -34,7 +59,7 @@ public class Solution {
                 NewInput.Add(Nums[i]);
                 
                 NewInput.AddRange(Input);
-                //NewInput.Sort();
+                NewInput.Sort();
 
                 Tree(Nums.Where(x => x != Nums[i]).ToList(), NewInput);
             }
