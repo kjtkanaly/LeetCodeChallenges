02574956182a7964633b305f255093ee3c73f30e
kjtkanaly <kjtkanaly@gmail.com>
2022-09-30 15:13:59 -0400

    WIP

--- CombinationSum.cs
+++ CombinationSum.cs
@@ -4,17 +4,29 @@ public class Solution {
     
     public IList<IList<int>> CombinationSum(int[] Cand, int target) {
         
+        // Debug
+        List<int> Test1 = new List<int>(3){1,2,3};
+        List<int> Test2 = new List<int>(3){1,2,3,4}; //{4,5,6};
+        
+        var One = Test1.Except(Test2).ToList();
+        var Two = Test2.Except(Test1).ToList();
+        
+        Console.WriteLine(One.Count + ", " + Two.Count);
+        
         List<int> Temp = Cand.ToList();
         Temp.Sort();
         Cand = Temp.ToArray();
         
         Tree(Cand, target, new List<int>());
         
+        // The probelm is in here with how I'm removing duplicates
         int Left = 0;
         while (Left < Result.Count)
         {
-            int Right = Left + 1;
-            while (Right < Result.Count)
+            Console.WriteLine(Result.Count);
+            
+            int Right = Result.Count - 1;
+            while (Right > Left)
             {
                 var ListOne = Result[Left].Except(Result[Right]).ToList();
                 var ListTwo = Result[Right].Except(Result[Left]).ToList();
@@ -23,14 +35,12 @@ public class Solution {
                 {
                     Result.RemoveAt(Right);
                 }
-                else
-                {
-                    Right++;
-                }
+                
+                Right--;
             }
             
             Left++;
-        }
+        }/**/
 
         return Result;
     }
@@ -39,22 +49,23 @@ public class Solution {
     {        
         for (int i = 0; i < Cand.Length; i++)
         {
-            int Current = target - Cand[i];
+            //int Current = target - Cand[i];
             
             List<int> Update = new List<int>(Prev.Count + 1);
             Update.AddRange(Prev);
             Update.Add(Cand[i]);
             
             Update.Sort();
+            Update.Reverse();
             
-            if (Current == 0)
+            if (Update.Sum() == target)
             {
                 Result.Add(Update);
-                return;
+                //return;
             }       
-            else if (Current > 0)
+            else if (Update.Sum() < target)
             {
-                Tree(Cand, Current, Update);
+                Tree(Cand, target, Update);
             }
             else
             {
