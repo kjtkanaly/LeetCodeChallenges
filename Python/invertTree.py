d3ecd02014ea45a26c76f154974f5fddbbe1a1a6
kjtkanaly <kjtkanaly@gmail.com>
2023-02-01 17:09:38 -0500

    FindMind and invertTree

---
+++ Python/invertTree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+
+        if (root == None):
+            return None
+
+        if (root.left != None):
+            self.invertTree(root.left)
+
+        if (root.right != None):
+            self.invertTree(root.right)
+
+        oldRight = root.right
+        root.right = root.left
+        root.left = oldRight
+
+        return root
